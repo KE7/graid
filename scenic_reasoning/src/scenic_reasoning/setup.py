6aeb5fde1a502888b364bdf75546cc1d88bd01e7
Karim Elmaaroufi <elmaaroufi@berkeley.edu>
2024-11-04 13:27:05 -0800

    organize subrepos installed into single folder

--- scenic_reasoning/src/scenic_reasoning/setup.py
+++ scenic_reasoning/src/scenic_reasoning/setup.py
@@ -1,3 +1,4 @@
+import argparse
 import os
 import platform
 import subprocess
@@ -9,6 +10,10 @@ def install_depth_pro():
     root_dir = PROJECT_DIR
 
     os.chdir(root_dir)
+    if not os.path.exists("install"):
+        os.makedirs("install")
+
+    os.chdir("install")
 
     # Check if the repository already exists in the root
     if not os.path.exists("ml-depth-pro"):
@@ -31,12 +36,33 @@ def install_depth_pro():
         subprocess.run(["bash", "ml-depth-pro/get_pretrained_models.sh"])
 
 
+def clean_depth_pro():
+    root_dir = PROJECT_DIR
+
+    os.chdir(root_dir)
+    if not os.path.exists("install"):
+        os.makedirs("install")
+
+    os.chdir("install")
+
+    # Check if the repository already exists in the root
+    if os.path.exists("ml-depth-pro"):
+        subprocess.run(["rm", "-rf", "ml-depth-pro"])
+
+    # Change back to the original directory
+    os.chdir("..")
+
+
 def install_detectron2():
     # https://detectron2.readthedocs.io/en/latest/tutorials/install.html
 
     root_dir = PROJECT_DIR
 
     os.chdir(root_dir)
+    if not os.path.exists("install"):
+        os.makedirs("install")
+
+    os.chdir("install")
 
     # Check if the repository already exists in the root
     if not os.path.exists("detectron2"):
@@ -69,6 +95,110 @@ def install_detectron2():
     os.chdir("..")
 
 
+def clean_detectron2():
+    root_dir = PROJECT_DIR
+
+    os.chdir(root_dir)
+    if not os.path.exists("install"):
+        return
+
+    os.chdir("install")
+
+    # Check if the repository already exists in the root
+    if os.path.exists("detectron2"):
+        subprocess.run(["rm", "-rf", "detectron2"])
+
+    # Change back to the original directory
+    os.chdir("..")
+
+
+def install_mmdetection():
+    subprocess.run(["pip", "install", "--upgrade", "openmim"])
+    subprocess.run(["mim", "install", "mmengine"])
+    subprocess.run(["mim", "install", "mmcv==2.1.0"])
+
+    root_dir = PROJECT_DIR
+
+    os.chdir(root_dir)
+
+    if not os.path.exists("install"):
+        os.makedirs("install")
+
+    os.chdir("install")
+
+    # Check if the repository already exists in the root
+    if not os.path.exists("mmdetection"):
+        subprocess.run(
+            ["git", "clone", "https://github.com/open-mmlab/mmdetection.git"]
+        )
+
+    # Change to the mmdetection directory
+    os.chdir("mmdetection")
+
+    subprocess.run(["pip", "install", "-e", "."])
+
+    # Change back to the original directory
+    os.chdir("..")
+
+
+def clean_mmdetection():
+    root_dir = PROJECT_DIR
+
+    os.chdir(root_dir)
+    if not os.path.exists("install"):
+        return
+
+    os.chdir("install")
+
+    # Check if the repository already exists in the root
+    if os.path.exists("mmdetection"):
+        subprocess.run(["rm", "-rf", "mmdetection"])
+
+    # Change back to the original directory
+    os.chdir("..")
+
+
+def download_nuscenes():
+    parser = argparse.ArgumentParser()
+    parser.add_argument(
+        "--size", type=str, default="mini", help="mini or full", required=True
+    )
+
+    args = parser.parse_args()
+    size = args.size
+    
+    root_dir = PROJECT_DIR
+
+    os.chdir(root_dir)
+
+    subprocess.run(["mkdir", "-p", "data/nuscenes"])
+
+    if size == "mini":
+            # !wget https://www.nuscenes.org/data/v1.0-mini.tgz  # Download the nuScenes mini split.
+
+            # !tar -xf v1.0-mini.tgz -C /data/sets/nuscenes  # Uncompress the nuScenes mini split.
+
+            # !pip install nuscenes-devkit &> /dev/null  # Install nuScenes.
+        location = "https://www.nuscenes.org/data/v1.0-mini.tgz"
+        if not subprocess.run(["wget", location]).returncode == 0:
+            raise OSError("wget is not installed on your system.")
+
+        subprocess.run(["tar", "-xf", "v1.0-mini.tgz", "-C", "data/nuscenes"])
+
+    elif size == "full":
+        raise NotImplementedError
+    
+
+    subprocess.run(["pip", "install", "nuscenes-devkit"])
+
+
 def install_all():
     install_depth_pro()
     install_detectron2()
+    install_mmdetection()
+
+
+def clean_all():
+    clean_depth_pro()
+    clean_detectron2()
+    clean_mmdetection()
