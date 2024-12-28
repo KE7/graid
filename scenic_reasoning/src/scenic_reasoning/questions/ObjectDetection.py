06d1e55af67668879f29e707ecf9f4704c231f7e
Karim Elmaaroufi <elmaaroufi@berkeley.edu>
2024-12-28 22:41:23 +0100

    IsObjectCentered row 9 done

--- scenic_reasoning/src/scenic_reasoning/questions/ObjectDetection.py
+++ scenic_reasoning/src/scenic_reasoning/questions/ObjectDetection.py
@@ -3,6 +3,7 @@ from PIL import Image
 from typing import Any, Callable, List, Tuple
 
 from scenic_reasoning.interfaces.ObjectDetectionI import ObjectDetectionResultI
+import torch
 
 
 class Question(ABC):
@@ -14,7 +15,10 @@ class Question(ABC):
         predicates: List[Callable],
         answer: Any,
     ) -> None:
-        pass
+        self.question = question
+        self.variables = variables
+        self.predicates = predicates
+        self.answer = answer
 
     def is_applicable(
         self,
@@ -31,7 +35,7 @@ class Question(ABC):
         Returns:
             bool: True if all predicates return True, False otherwise.
         """
-        return all(predicate(detections) for predicate in self.predicates)
+        return all(predicate(image, detections) for predicate in self.predicates)
     
     # TODO: cache the results of the predicates to avoid recomputing them.
     #   because we need them when converting the question to a string.
@@ -65,4 +69,82 @@ class Question(ABC):
         pass
 
 
+class ObjectDetectionPredicates:
+    @staticmethod
+    def at_least_one_single_detection(image, detections) -> bool:
+        counts = {}
+        for detection in detections:
+            class_name = detection.label
+            if type(class_name) == torch.Tensor: # shape == (# of boxes,)
+                # need to iterate over the tensor to get the class names
+                for class_name in class_name:
+                    counts[class_name] = counts.get(class_name, 0) + 1
+            else:
+                counts[class_name] = counts.get(class_name, 0) + 1
+        
+        return any(count == 1 for count in counts.values())
 
+
+class IsObjectCentered(Question):
+    def __init__(self) -> None:
+        super().__init__(
+            question="Is <object_1> centered in the image, or is it off to the left or right?",
+            variables=["object_1"],
+            predicates=[
+                ObjectDetectionPredicates.at_least_one_single_detection,
+            ],
+        )
+
+    def apply(
+        self,
+        image: Image.Image,
+        detections: List[ObjectDetectionResultI],
+    ) -> List[Tuple[str, str]]:
+        # @precondition: at_least_one_single_detection(image, detections) == True
+
+        # get all the classes that have only one detection
+        detection_counts = {}
+        for detection in detections:
+            class_name = detection.label
+            if type(class_name) == torch.Tensor: # shape == (# of boxes,)
+                # need to iterate over the tensor to get the class names
+                for class_name in class_name:
+                    detection_counts[class_name] = detection_counts.get(class_name, 0) + 1
+            else:
+                detection_counts[class_name] = detection_counts.get(class_name, 0) + 1
+        
+        single_detections = [class_name for class_name, count in detection_counts.items() if count == 1]
+
+        image_width, image_height = image.size
+
+        object_positions = []
+        for detection in detections:
+            class_name = detection.label
+            if type(class_name) == torch.Tensor: # shape == (# of boxes,)
+                # need to iterate over the tensor to get the class names
+                for class_name in class_name:
+                    if class_name in single_detections:
+                        object_positions.append((class_name, detection.as_xyxy()[0][0], detection.as_xyxy()[0][2]))
+            else:
+                if class_name in single_detections:
+                    object_positions.append((class_name, detection.as_xyxy()[0][0], detection.as_xyxy()[0][2]))
+
+        question_answer_pairs = []
+        for class_name, x_min, x_max in object_positions:
+            question = f"Is {class_name} centered in the image, or is it off to the left or right?"
+            
+            # TODO: verify this design decision manually
+            # edge case: if the object is big enough to cover more than 1/3rd
+            # the answer should be left or right
+            if x_min < image_width / 3 and x_max < 2 * image_width / 3:
+                answer = "left"
+            elif x_min > image_width / 3 and x_max < 2 * image_width / 3:
+                answer = "centered"
+            elif x_min > image_width / 3 and x_max > 2 * image_width / 3:
+                answer = "right"
+            else:
+                # x_min < image_width / 3 and x_max > 2 * image_width / 3
+                answer = "centered"
+            question_answer_pairs.append((question, answer))
+
+        return question_answer_pairs
