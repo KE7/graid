072fd6ba9c81d3635791f52e5bad5826989d96ab
Karim Elmaaroufi <elmaaroufi@berkeley.edu>
2025-10-24 18:59:57 -0700

    website

--- clear_notebook.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-set -euo pipefail
-
-GREEN='\033[0;32m'
-NC='\033[0m' # No Color
-echo -e "${GREEN}Clearing outputs from all notebooks${NC}"
-exec jupyter-nbconvert --clear-output --inplace notebooks/*.ipynb
